701cd34297060bfd8be91c50d79a8a0b52b92391
Marzena Studniarz <marzena.kupiec@gmail.com>
2020-12-22 01:08:17 +0100

    [searchByFirstLetter - refactor]

--- src/components/SearchByFirstLetter/SearchByFirstLetter.js
+++ src/components/SearchByFirstLetter/SearchByFirstLetter.js
@@ -31,10 +31,17 @@ const SearchByFirstLetter = () => {
 
 
     useEffect(() => {
-        handleDataFetching();
+        handleDataFetching(firstLetter);
     }, [firstLetter]);
 
-    
+    const allLetters = 'ABCDEFGHIJKLMNOPQRSTUVWYZ';
+    const oneLetter = allLetters.split('');
+    const setOneLetter = oneLetter.map(letter => (
+        <button 
+            onClick={() => {setFirstLetter(letter)}}>
+            {letter}
+        </button>
+    ));
 
     return (
         <div>
@@ -43,44 +50,7 @@ const SearchByFirstLetter = () => {
 
             <div>
                 <ul>
-                    <button 
-                        onClick={() => {setFirstLetter('a')}}>
-                            A
-                    </button>
-
-                    <button 
-                        onClick={() => {setFirstLetter('b')}}>
-                            B
-                    </button>
-
-                    <button 
-                        onClick={() => {setFirstLetter('c')}}>
-                            C
-                    </button>                   
-
-                    <button>D</button>
-                    <button>E</button>
-                    <button>F</button>
-                    <button>G</button>
-                    <button>H</button>
-                    <button>I</button>
-                    <button>J</button>
-                    <button>K</button>
-                    <button>L</button>
-                    <button>M</button>
-                    <button>N</button>
-                    <button>O</button>
-                    <button>P</button>
-                    <button>Q</button>
-                    <button>R</button>
-                    <button>S</button>
-                    <button>T</button>
-                    <button>U</button>
-                    <button>V</button>
-                    <button>W</button>
-                    <button>X</button>
-                    <button>Y</button>
-                    <button>Z</button>
+                    {setOneLetter}
                 </ul>
             </div>
 
