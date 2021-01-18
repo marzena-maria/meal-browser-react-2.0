b8493695f951491bad1f8b8a17cc6c1ac5993806
Marzena Studniarz <marzena.kupiec@gmail.com>
2021-01-18 21:20:53 +0100

    Updates

---
+++ precache-manifest.88ede39520ada23456604f33941c14b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7de90dad753be5808876890f54b5c05",
+    "url": "/meal-browser-react-2.0/index.html"
+  },
+  {
+    "revision": "a3e0ff72d70d836da837",
+    "url": "/meal-browser-react-2.0/static/css/main.a2dc8759.chunk.css"
+  },
+  {
+    "revision": "d843b6c957c500a6d4b9",
+    "url": "/meal-browser-react-2.0/static/js/2.cf46d82e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/meal-browser-react-2.0/static/js/2.cf46d82e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e0ff72d70d836da837",
+    "url": "/meal-browser-react-2.0/static/js/main.7fb8c99f.chunk.js"
+  },
+  {
+    "revision": "147628b4f153efa69dbd",
+    "url": "/meal-browser-react-2.0/static/js/runtime-main.115d3424.js"
+  }
+]);
